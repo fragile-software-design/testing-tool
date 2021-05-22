5c68f26cf7bfcd85ca346f616fac998f88488f62
Ni Putu Intan Maharani <npim550@uowmail.edu.au>
2021-05-22 16:24:43 +1000

    Automated test case generator refactored to be in separate functions and in separate .ts file.

--- oracle/src/index.ts
+++ oracle/src/index.ts
@@ -1,38 +1,9 @@
-import faker from 'faker';
+//import faker from 'faker';
+import { TestCase } from './testCaseGenerator.js';
 import { Builder, By, until } from 'selenium-webdriver';
 import 'chromedriver';
 
-function generateDestinationName (){
-    let destination = faker.address.cityName();//Generate city name randomly
-    return destination;
-}
-
-function changeDateFormat (date){
-    var year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
-    var month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
-    var day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
-    var newFormat = `${year}-${month}-${day}`;
-    return newFormat;
-}
-
-function generateDates (){
-    /*CHECKIN DATE*/
-    const today = new Date();
-    var checkinDate = new Date(today);
-    checkinDate.setDate(checkinDate.getDate() + 1);//set checkin date to tommorrow (feel free to change)
-    var checkinDateInput = changeDateFormat(checkinDate);
-
-    /*CHECKOUT DATE*/
-    var stayDuration = Math.floor((Math.random() * 10) + 1);//stay duration from 1 day to 10 days (feel free to change)
-    var checkoutDate = new Date (checkinDate);
-    checkoutDate.setDate(checkoutDate.getDate() + stayDuration);
-    var checkoutDateInput = changeDateFormat(checkoutDate);
-
-    return {
-        checkinDateInput,
-        checkoutDateInput
-    };
-}
+const testCase = new TestCase();
 
 const exampleTest = async (): Promise<void> => {
   const driver = await new Builder().forBrowser('chrome').build();
@@ -63,38 +34,39 @@ const exampleTest = async (): Promise<void> => {
   //************************************************************************************************************************
   //2.1 TEST-2 ENTERING TEST CASE
   //2.1.1 Enter the destination name
-  const destinationInput = await driver.findElement(By.name('ss'));
-  await destinationInput.sendKeys(generateDestinationName());
+  const destinationInput = testCase.generateDestinationName();
+  await driver.findElement(By.name('ss')).sendKeys(destinationInput);
   
   //2.1.2 Enter the checkin and checkout dates
   const dates = await driver.findElement(By.className("xp__dates-inner"));
   await dates.click();//Click dates box
-  await driver.findElement(By.css("td.bui-calendar__date[data-date='"+generateDates().checkinDateInput+"']")).click();//checkin
-  await driver.findElement(By.css("td.bui-calendar__date[data-date='"+generateDates().checkoutDateInput+"']")).click();//checkout
+  const checkin = testCase.generateDates().checkinDateInput;
+  const checkout = testCase.generateDates().checkoutDateInput;
+  await driver.findElement(By.css("td.bui-calendar__date[data-date='"+checkin+"']")).click();//checkin
+  await driver.findElement(By.css("td.bui-calendar__date[data-date='"+checkout+"']")).click();//checkout
   
   //2.1.3 Enter the guest details (automated case?)
-  const guestDetails = await driver.findElement(By.className("xp__input"));
-  await guestDetails.click();//Click the guest details box
+  await driver.findElement(By.className("xp__input")).click();//Click the guest details box
 
   //2.1.3.1 No of Adults
-  var noOfAdults = Math.floor((Math.random()*10)+1);//random number from 1-10
-  var defaultAdults = 2;
+  const noOfAdults = testCase.generateNoOfAdults();
+  const defaultAdults = 2;
 
   if (noOfAdults < defaultAdults) {
       await driver.findElement(By.css("button[aria-label='Decrease number of Adults']")).click();
   } else if (noOfAdults > defaultAdults) {
-      var i;
+      let i;
       for (i = 0; i < noOfAdults-2; i++) {
           await driver.findElement(By.css("button[aria-label='Increase number of Adults']")).click();
       }
   }
 
   //2.1.3.2 No of Children
-  var noOfChildren = Math.floor((Math.random()*10));//random number from 0-10
-  var defaultChildren = 0;
+  const noOfChildren = testCase.generateNoOfChildren();
+  const defaultChildren = 0;
 
   if(noOfChildren > defaultChildren){
-      var i;
+      let i;
       for (i = 0; i < noOfChildren; i++) {
           await driver.findElement(By.css("button[aria-label='Increase number of Children']")).click();
       }
@@ -112,12 +84,13 @@ const exampleTest = async (): Promise<void> => {
 
   //2.2 TEST-2 RESULT CHECKING (TEST ORACLE, maybe test more)
   try {
-      await driver.wait(until.titleContains(generateDestinationName()), 5000);//Check the title
-      console.log("Test-2 Result: Passed! Successfully load hotels in "+generateDestinationName());
+      await driver.wait(until.titleContains(destinationInput), 5000);//Check the title
+      console.log("Test-2 Result: Passed! Successfully load hotels in "+destinationInput);
   } catch (e) {
-      console.log("Test-2 Result: Failed! Failed to load hotels in "+generateDestinationName());
+      console.log("Test-2 Result: Failed! Failed to load hotels in "+destinationInput);
   }
 
+    console.log(destinationInput,checkin,checkout,noOfAdults,noOfChildren);
   //************************************************************************************************************************
   //await driver.quit();
 
